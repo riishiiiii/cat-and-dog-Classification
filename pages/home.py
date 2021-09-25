deb85fcdf5300fc1533678c876489949779e6ddb
Rishi Pandey <49920368+riishiiiii@users.noreply.github.com>
2021-09-25 23:25:52 +0530

    Add files via upload

---
+++ pages/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def app():
+
+    st.write("""
+            # Cat and Dog Classification
+            """
+             )
+    st.image("catordog .png", width=600)
---
+++ pages/prediction.py
@@ -0,0 +1,31 @@
+from model import load_model
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+import keras.preprocessing
+
+
+def app():
+    with st.spinner('Model is being loaded..'):
+        model = load_model()
+
+    file = st.file_uploader(
+        "Please upload a Picture of CAT or DOG ", type=["jpg", "png"])
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+
+    if file is None:
+        st.text("Please upload an image file")
+    else:
+        pic = Image.open(file)
+        # pic_size = pic.resize((100, 100))
+        st.image(pic, use_column_width=True)
+        test_img = tf.image.resize(pic, [64, 64])
+        img = keras.preprocessing.image.img_to_array(test_img)
+        img = np.expand_dims(img, axis=0)
+        r = model.predict(img)
+        if r[0][0] == 1:
+            pred = "It's a DOG"
+        else:
+            pred = "It's a CAT"
+        st.success(pred)
