4a03b62f593610cdc0703334400636ccfb72cdcd
Rishi Pandey <49920368+riishiiiii@users.noreply.github.com>
2021-10-10 18:25:06 +0530

    Add files via upload

---
+++ pages/about.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def app():
+
+    st.markdown(
+        '''### GitHub Repository: [cat-and-dog-Classification](https://github.com/riishiiiii/cat-and-dog-Classification)''')
+
+    st.markdown(
+        '''### Source code: [Notebook](https://www.kaggle.com/riishiiiii/cat-and-dog-classfication?scriptVersionId=76760469)''')
