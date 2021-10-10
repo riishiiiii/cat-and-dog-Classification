6754361402698086d39d1b5ddf713bce9a1826a2
Rishi Pandey <49920368+riishiiiii@users.noreply.github.com>
2021-10-10 15:25:19 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('catvsdog.h5')
+  model=tf.keras.models.load_model('catdog.h5')
   return model
