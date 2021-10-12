919011aae77b462475efaaf638520585d752f416
Rishi Pandey <49920368+riishiiiii@users.noreply.github.com>
2021-10-12 22:21:19 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -3,8 +3,15 @@ import streamlit as st
 
 def app():
 
-    st.write("""
-            # Cat and Dog Classification
+    st.title("""
+            Cat and Dog Classification
             """
              )
     st.image("catordog .png")
+
+    st.markdown("""
+        **To predict go to prediction page and uplode an image.** """)
+
+    st.write("""
+        **Github Repository and Source code is available on About page.**
+            """)
