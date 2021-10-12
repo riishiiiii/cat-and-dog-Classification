56ec74f19382e9a91274170652e58fe8dd2eaeb9
Rishi Pandey <49920368+riishiiiii@users.noreply.github.com>
2021-10-12 22:22:01 +0530

    Update prediction.py

--- pages/prediction.py
+++ pages/prediction.py
@@ -4,32 +4,38 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 import keras.preprocessing
+import cv2
 
 
 def app():
     with st.spinner('Model is being loaded..'):
         model = load_model()
-
+    st.subheader("""
+    **NOTE:** This app work best only when you uplode image of a Cat or Dog.
+    """)
     file = st.file_uploader(
         "Please upload a Picture of CAT or DOG ", type=["jpg", "jpeg", "png"])
     st.set_option('deprecation.showfileUploaderEncoding', False)
 
     if file is None:
-        st.text("Please upload an image file")
+        st.write("""
+        Please upload an image file
+        """)
     else:
-        try:
-            pic = Image.open(file)
-            # pic_size = pic.resize((100, 100))
-            st.image(pic, use_column_width=True)
-            test_img = tf.image.resize(pic, [64, 64])
-            img = keras.preprocessing.image.img_to_array(test_img)
-            img = np.expand_dims(img, axis=0)
+        if st.button("Predict"):
+            try:
+                pic = Image.open(file)
+                # pic_size = pic.resize((100, 100))
+                st.image(pic, use_column_width=True)
+                test_img = tf.image.resize(pic, [64, 64])
+                img = keras.preprocessing.image.img_to_array(test_img)
+                img = np.expand_dims(img, axis=0)
 
-            r = model.predict(img)
-            if r[0][0] == 1:
-                pred = "It's a DOG"
-            else:
-                pred = "It's a CAT"
-            st.success(pred)
-        except:
-            st.error("Invalid Image Type For This Model")
+                r = model.predict(img)
+                if r[0][0] == 1:
+                    pred = "It's a DOG"
+                else:
+                    pred = "It's a CAT"
+                st.success(pred)
+            except:
+                st.error("Invalid Image Type For This Model")
